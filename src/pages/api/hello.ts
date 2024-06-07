30003b0025022efd8fda97811c8a75c198dfdbe0
kyimoemin <kyimoemin0@gmail.com>
2024-06-07 17:33:57 +0700

    added Server Sent Events

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,7 +7,33 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Content-Encoding", "none");
+  res.flushHeaders();
+
+  const senderId = req.query?.id;
+
+  function sendEvent(message: string) {
+    // the text has to start with `data:` and end with `\n\n`
+    const text = `data: ${JSON.stringify({ senderId, message })}\n\n`;
+    res.write(text, (err) => {
+      if (err) console.log("sending failed", err);
+    });
+  }
+
+  sendEvent("event streaming initiated");
+
+  let i = 1;
+  const id = setInterval(() => {
+    sendEvent(`hello world ${i++}`);
+  }, 1000);
+
+  res.on("close", () => {
+    clearInterval(id);
+    res.end();
+  });
 }
